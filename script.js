192dcbee01e2f1f6d46fe4709c2422e62da75653
victorstdev <victorstdev@gmail.com>
2025-02-03 11:39:54 -0300

    update colors

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const inicioDoExpediente = new Date();
 const fimDoExpediente = new Date();
 const result = document.getElementById("texto");
 const icon = document.getElementById("icon");
+const box = document.getElementById("box");
 let texto = "";
+let rgbValue = "";
+let rgbBackground = "";
 
 inicioDoExpediente.setHours(8, 0, 0, 0);
 fimDoExpediente.setHours(17, 0, 0, 0);
@@ -12,31 +15,34 @@ const tempoDeExpediente = Math.floor((fimDoExpediente - inicioDoExpediente) / (1
 const tempoAteJaTaPodendo = Math.floor((fimDoExpediente - now) / (1000 * 60));
 const taxaDeJaTaPodendo = Math.round((1 - tempoAteJaTaPodendo/tempoDeExpediente)*100);
 
-const r = taxaDeJaTaPodendo < 50 ? 255 : Math.floor(255 - (taxaDeJaTaPodendo - 50) * 5.1);
-const g = taxaDeJaTaPodendo < 50 ? Math.floor(taxaDeJaTaPodendo * 5.1) : 255;
-const b = 0;
-const rgbValue = `rgb(${r},${g},${b})`;
-
-const trocarIcone = (className) => {
+const trocarIcone = (className, cor) => {
   icon.className = className;
   icon.style.display = 'none';
   setTimeout(() => {
     icon.style.display = 'inline-block';
-    icon.style.color = rgbValue;
+    icon.style.color = cor;
   }, 10);
 }
 
-// result.style.color = rgbValue;
 
 if (taxaDeJaTaPodendo >= 100) {
   texto = "Já pode!";
-  trocarIcone("fi fi-rr-social-network");
-}else if (taxaDeJaTaPodendo >= 50) {
-  texto = "Tá quase podendo! Faltam " + tempoAteJaTaPodendo + " minutos.";
-  trocarIcone("fi fi-rr-hand-sparkles");
+  rgbValue = "#2e7d32";
+  rgbBackground = "#e8f5e5";
+  trocarIcone("fi fi-rr-social-network", rgbBackground);
+}else if (taxaDeJaTaPodendo >= 80) {
+  texto = "Calma que já tá quase podendo! Faltam " + tempoAteJaTaPodendo + " minutos.";
+  rgbValue = "#f9a825";
+  rgbBackground = "#fdecc0";
+  trocarIcone("fi fi-rr-hand-sparkles", rgbBackground);
 }else{
   texto = "Ainda não pode! Faltam " + tempoAteJaTaPodendo + " minutos.";
-  trocarIcone("fi fi-rr-hand");
+  rgbValue = "#b71c1c";
+  rgbBackground = "#f4cec2";
+  trocarIcone("fi fi-rr-hand", rgbBackground);
 }
 
+box.style.backgroundColor = rgbValue;
+box.style.color = rgbBackground;
+result.style.color = rgbBackground;
 result.innerHTML = texto;
