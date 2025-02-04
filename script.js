ca41a9fbfba4d6083c9ff845a0198fb4883254ee
victorstdev <victorstdev@gmail.com>
2025-02-04 14:41:14 -0300

    Atualizei resultado para hora e minuto

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ fimDoExpediente.setHours(17, 0, 0, 0);
 const tempoDeExpediente = Math.floor((fimDoExpediente - inicioDoExpediente) / (1000 * 60));
 const tempoAteJaTaPodendo = Math.floor((fimDoExpediente - now) / (1000 * 60));
 const taxaDeJaTaPodendo = Math.round((1 - tempoAteJaTaPodendo/tempoDeExpediente)*100);
+const tempoEmHHMM = `${Math.floor(tempoAteJaTaPodendo / 60)}h ${tempoAteJaTaPodendo % 60}m`
 
 const trocarIcone = (className) => {
   icon.className = className;
@@ -40,9 +41,9 @@ if (taxaDeJaTaPodendo >= 100) {
   resultado("Já pode!", "bg-success-subtle", "btn-success", "bg-success");
   trocarIcone("fi fi-rr-social-network display-3 text-success");
 }else if (taxaDeJaTaPodendo >= 80) {
-  resultado(`Calma que já tá quase podendo! Faltam ${tempoAteJaTaPodendo} minutos.`, "bg-warning-subtle", "btn-warning", "bg-warning");
+  resultado(`Calma que já tá quase podendo! Faltam ${tempoEmHHMM}.`, "bg-warning-subtle", "btn-warning", "bg-warning");
   trocarIcone("fi fi-rr-hand-sparkles display-3 text-warning");
 }else{
-  resultado(`Ainda não pode! Faltam ${tempoAteJaTaPodendo} minutos.`, "bg-danger-subtle", "btn-danger", "bg-danger");
+  resultado(`Ainda não pode! Faltam ${tempoEmHHMM}.`, "bg-danger-subtle", "btn-danger", "bg-danger");
   trocarIcone("fi fi-rr-hand display-3 text-danger");
 }
